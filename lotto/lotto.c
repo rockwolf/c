f6729d5533cbd6c3141e11a8cf22b60385ca24ea
rockwolf <thereisanewway@gmail.com>
2013-12-20 14:02:16 +0100

    Check if number is taken.

--- lotto/lotto.c
+++ lotto/lotto.c
@@ -8,8 +8,29 @@
 #include <time.h>
 
 int rand_lim(int limit);
+int number_is_taken(int n, int n_array[])
 
-/* return a random number between 0 and limit inclusive. */
+int main(int argc, char *argv[])
+{
+    int i;
+    int numbers[NUM] = {0};
+	
+    srand(time(NULL));
+    for(i = 0; i<NUM;)
+    {
+        int r = rand_lim(RAND_LIMIT);
+        if(number_is_taken(r, numbers) = 0)
+        {
+    	    numbers[i] = r;
+    	    i++;
+        }
+        printf("%d ", r);
+    }
+    printf("\b");
+    return EXIT_SUCCESS;
+}
+
+/* Returns a random number between 0 and limit inclusive. */
 int rand_lim(int limit)
 {
     int divisor = RAND_MAX/(limit+1);
@@ -23,16 +44,13 @@ int rand_lim(int limit)
     return retval+1;
 }
 
-int main(int argc, char *argv[])
+/* Checks if the chosen int is in the given int array. */
+int number_is_taken(int n, int n_array[])
 {
-	int i;
-	
-	srand(time(NULL));
-	for(i = 0; i<NUM; i++)
-	{
-		int r = rand_lim(RAND_LIMIT);
-		printf("%d ", r);
-	}
-	printf("\b");
-	return EXIT_SUCCESS;
+    int i, result;
+    for (i=0; i<NUM; i++)
+    {
+    	result = (n_array[i] == n) ? 1 : 0; 
+    }
+    return result
 }
