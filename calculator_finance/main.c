aa5febe8a3e58e5ed6f05857fa89c8268c86e170
rockwolf <thereisanewway@gmail.com>
2015-04-03 14:18:58 +0200

    Create main.c

---
+++ calculator_finance/main.c
@@ -0,0 +1,12 @@
+/* See LICENSE.txt file for license and copyright information. */
+
+#include <stdio.h>
+#include "calculator_finance.c"
+#include "unit_test.c"
+
+int main(int argc, char* argv[])
+{
+    // Tests go here.;
+    test_calculate_percentage_of();
+    return 0;
+}
