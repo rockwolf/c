af253e60be4559506c6ec945753e2cef3deb41f4
rockwolf <thereisanewway@gmail.com>
2015-04-03 14:19:58 +0200

    Update unit_test.c

--- calculator_finance/unit_test.c
+++ calculator_finance/unit_test.c
@@ -2,4 +2,11 @@
 
 #include <stdio.h>
 
-//TODO: the unit-tests go here.
+int test_calculate_percentage_of();
+
+int test_calculate_percentage_of()
+{
+    double result;
+    result = calculate_percentage_of(2.0, 50.0); 
+    printf("calculate_percentage_of(2.0, 50.0) = %.2f [%s]\n", result, bool_to_success(result == 4.0));
+}
