71b663a6047824b2ddd8744457f8bbca7b60a31c
rockwolf <thereisanewway@gmail.com>
2015-04-07 14:40:01 +0200

    Added test_cost_transaction.

--- calculator_finance/unit_test_trade.c
+++ calculator_finance/unit_test_trade.c
@@ -14,8 +14,8 @@
 char* test_calculate_percentage_of()
 {
     mu_assert(
-            "[FAIL] Wrong result for calculate_percentage_of(2.0, 50.0)!",
-            calculate_percentage_of(2.0, 50.0) == 4.0
+        "[FAIL] Wrong result for calculate_percentage_of(2.0, 50.0)!",
+        calculate_percentage_of(2.0, 50.0) == 4.0
     ); 
     return 0;
 }
@@ -32,8 +32,8 @@ char* test_convert_from_orig()
 char* test_convert_to_orig()
 {
     mu_assert(
-            "[FAIL] Wrong result for convert_to_orig(12.0, 0.5)!",
-            convert_from_orig(12.0, 0.5) == 6.0
+        "[FAIL] Wrong result for convert_to_orig(12.0, 0.5)!",
+        convert_from_orig(12.0, 0.5) == 6.0
     ); 
     return 0;
 }
@@ -43,12 +43,12 @@ char* test_convert_to_orig()
 char* test_calculate_stoploss()
 {
     mu_assert(
-            "[FAIL] -short- Wrong result for calculate_stoploss(12.0, 2, 3.0, 1.0, 2.0, 10000.0, 0)!",
-            equals(calculate_stoploss(12.0, 2, 3.0, 1.0, 2.0, 10000.0, 0), 109.004926, C_PRECISION)
+        "[FAIL] -short- Wrong result for calculate_stoploss(12.0, 2, 3.0, 1.0, 2.0, 10000.0, 0)!",
+        equals(calculate_stoploss(12.0, 2, 3.0, 1.0, 2.0, 10000.0, 0), 109.004926, C_PRECISION)
     ); 
     mu_assert(
-            "[FAIL] -long- Wrong result for calculate_stoploss(12.0, 2, 3.0, 1.0, 2.0, 10000.0, 1)!",
-            equals(calculate_stoploss(12.0, 2, 3.0, 1.0, 2.0, 10000.0, 1), -87.9593908629, C_PRECISION)
+        "[FAIL] -long- Wrong result for calculate_stoploss(12.0, 2, 3.0, 1.0, 2.0, 10000.0, 1)!",
+        equals(calculate_stoploss(12.0, 2, 3.0, 1.0, 2.0, 10000.0, 1), -87.9593908629, C_PRECISION)
     ); 
     return 0;
 }
@@ -56,8 +56,8 @@ char* test_calculate_stoploss()
 char* test_calculate_risk_input()
 {
     mu_assert(
-            "[FAIL] Wrong result for calculate_risk_input(10000.0, 2.0)!",
-            equals(calculate_risk_input(10000.0, 2.0), 200.0, C_PRECISION)
+        "[FAIL] Wrong result for calculate_risk_input(10000.0, 2.0)!",
+        equals(calculate_risk_input(10000.0, 2.0), 200.0, C_PRECISION)
     ); 
     return 0;
 }
@@ -65,8 +65,8 @@ char* test_calculate_risk_input()
 char* test_calculate_risk_initial()
 {
     mu_assert(
-            "[FAIL] -short- Wrong result for calculate_risk_initial(12.0, 2, 3.0, 1.0, 2.0, 0)!",
-            equals(calculate_risk_initial(12.0, 2, 3.0, 1.0, 10.0, 0), -0.68, C_PRECISION)
+        "[FAIL] -short- Wrong result for calculate_risk_initial(12.0, 2, 3.0, 1.0, 2.0, 0)!",
+        equals(calculate_risk_initial(12.0, 2, 3.0, 1.0, 10.0, 0), -0.68, C_PRECISION)
     ); 
     mu_assert(
             "[FAIL] -long- Wrong result for calculate_risk_initial(12.0, 2, 3.0, 1.0, 2.0, 1)!",
@@ -78,8 +78,8 @@ char* test_calculate_risk_initial()
 char* test_calculate_amount()
 {
     mu_assert(
-            "[FAIL] Wrong result for calculate_amount(12.0, 2)!",
-            calculate_amount(12.0, 2) == 24.0
+        "[FAIL] Wrong result for calculate_amount(12.0, 2)!",
+        calculate_amount(12.0, 2) == 24.0
     ); 
     return 0;
 }
@@ -89,13 +89,13 @@ char* test_calculate_amount_with_tax_and_commission()
     transaction_type_t a_transaction_type_t;
     a_transaction_type_t = BUY;
     mu_assert(
-            "[FAIL] -buy- Wrong result for calculate_amount_with_tax_and_commission(12.0, 2, 3.0, 1.0, BUY)!",
-            equals(calculate_amount_with_tax_and_commission(12.0, 2, 3.0, 1.0, a_transaction_type_t), 97.0, C_PRECISION)
+        "[FAIL] -buy- Wrong result for calculate_amount_with_tax_and_commission(12.0, 2, 3.0, 1.0, BUY)!",
+        equals(calculate_amount_with_tax_and_commission(12.0, 2, 3.0, 1.0, a_transaction_type_t), 97.0, C_PRECISION)
     ); 
     a_transaction_type_t = SELL;
     mu_assert(
-            "[FAIL] -sell- Wrong result for calculate_amount_with_tax_and_commission(12.0, 2, 3.0, 1.0, SELL)!",
-            equals(calculate_amount_with_tax_and_commission(12.0, 2, 3.0, 1.0, a_transaction_type_t), -49.0, C_PRECISION)
+        "[FAIL] -sell- Wrong result for calculate_amount_with_tax_and_commission(12.0, 2, 3.0, 1.0, SELL)!",
+        equals(calculate_amount_with_tax_and_commission(12.0, 2, 3.0, 1.0, a_transaction_type_t), -49.0, C_PRECISION)
     ); 
     return 0;
 }
@@ -105,14 +105,23 @@ char* test_calculate_amount_with_tax()
     transaction_type_t a_transaction_type_t;
     a_transaction_type_t = BUY;
     mu_assert(
-            "[FAIL] -buy- Wrong result for calculate_amount_with_tax(12.0, 2, 3.0, BUY)!",
-            equals(calculate_amount_with_tax(12.0, 2, 3.0, a_transaction_type_t), 23.28, C_PRECISION)
+        "[FAIL] -buy- Wrong result for calculate_amount_with_tax(12.0, 2, 3.0, BUY)!",
+        equals(calculate_amount_with_tax(12.0, 2, 3.0, a_transaction_type_t), 23.28, C_PRECISION)
     ); 
     a_transaction_type_t = SELL;
     mu_assert(
             "[FAIL] -sell- Wrong result for calculate_amount_with_tax(12.0, 2, 3.0, SELL)!",
             equals(calculate_amount_with_tax(12.0, 2, 3.0, a_transaction_type_t), 24.72, C_PRECISION)
     ); 
+    return 0;
+}
+
+char* test_cost_transaction()
+{
+    mu_assert(
+        "[FAIL] Wrong result for cost_transaction(12.0, 2, 3.0, 1.0)!",
+        cost_transaction(12.0, 2, 3.0, 1.0) == 1.72
+    );
     return 0;
 }
 
@@ -127,5 +136,6 @@ char* test_all_trade()
     mu_run_test(test_calculate_amount);
     mu_run_test(test_calculate_amount_with_tax_and_commission);
     mu_run_test(test_calculate_amount_with_tax);
+    mu_run_test(test_cost_transaction);
     return 0;
 }
