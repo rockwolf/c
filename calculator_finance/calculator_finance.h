03528dc956043eb230e98f19c8ab9d850007da64
rockwolf <thereisanewway@gmail.com>
2015-04-13 16:50:36 +0200

    Added calculate_risk_actual

--- calculator_finance/calculator_finance.h
+++ calculator_finance/calculator_finance.h
@@ -19,3 +19,9 @@ double calculate_amount_with_tax(double a_price, int a_shares, double a_tax, tra
 double cost_transaction(double a_price, int a_shares, double a_tax, double a_commission);
 double cost_tax(double a_amount, double a_commission, int a_shares, double a_price, transaction_type_t a_transaction_type);
 double calculate_price(double a_amount, int a_shares, double a_tax, double a_commission, transaction_type_t a_transaction_type);
+
+// After trade
+
+double calculate_risk_actual(double_a_price_buy, int a_shares_buy, double a_tax_buy,
+    double a_commission_buy, double a_price_sell, int a_shares_sell, double a_tax_sell,
+    double a_commission_sell, double a_risk_initial, double a_profit_loss);
