80e6ac76972d326f3459135f8e8f084fb2a6189f
rockwolf <thereisanewway@gmail.com>
2015-04-30 16:00:48 +0200

    Moved to unit_test for now.

--- calculator_finance/unit_test/general_functions.h
+++ calculator_finance/unit_test/general_functions.h
@@ -1,5 +1,10 @@
 /* See LICENSE.txt file for license and copyright information. */
 
+#ifndef general_functions_h__
+#define general_functions_h__
+
 char* bool_to_success(_Bool a_bool);
 char* bool_to_string(_Bool a_bool);
 int equals(double d1, double d2, double precision);
+
+#endif  // general_functions_h__
