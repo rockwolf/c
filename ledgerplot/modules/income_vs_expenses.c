1fb15136a8a963049c56ecda2a3dd54e6ab339d9
rockwolf <thereisanewway@gmail.com>
2015-11-17 14:52:59 +0100

    refactor to modules

--- ledgerplot/modules/income_vs_expenses.c
+++ ledgerplot/modules/income_vs_expenses.c
@@ -1,1 +1,7 @@
 /* See LICENSE file for copyright and license info. */
+
+#define FILE_IVE_LAYOUT "gp_income_vs_expenses.gnu"
+
+
+static char *f_cmd_income_vs_expenses =
+    "ledger -f %s bal --real -X EUR -s -p %d -d \"T&l<=1\" expenses income | grep -Eo '[0-9\\.]{1,100}'";
