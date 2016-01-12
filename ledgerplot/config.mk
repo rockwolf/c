7c82211240355cb37e297ee11e6ccbdac0e5aada
rockwolf <thereisanewway@gmail.com>
2016-01-12 21:56:14 +0100

    Cleanup and testing. pedantic -> Wpedantic.

--- ledgerplot/config.mk
+++ ledgerplot/config.mk
@@ -20,7 +20,7 @@ LIBS =
 #CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\"
 CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c11 -Wpedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = -s ${LIBS}
 
 # Solaris
