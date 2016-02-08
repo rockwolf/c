da6c10aa419f9a52377c1efd8fb3ed77c284a01f
rockwolf <thereisanewway@gmail.com>
2016-02-08 08:42:24 +0100

    Get full command function.

--- ledgerplot/modules/income_vs_expenses.c
+++ ledgerplot/modules/income_vs_expenses.c
@@ -9,9 +9,9 @@
 #include "income_vs_expenses.h"
 
 
-static char *f_cmd_income_vs_expenses_yearly =
+static char *f_cmd_yearly =
     "ledger -f %s --strict bal --real -X EUR -s -p %d -d \"T&l<=1\" expenses income | grep -Eo '[-0-9][0-9\\.]{1,100}'";
-static char *f_cmd_income_vs_expenses_monthly =
+static char *f_cmd_monthly =
     "ledger -f %s --strict bal --real -X EUR -s -p \"%s %d\" -d \"T&l<=1\" expenses income | grep -Eo '[-0-9][0-9\\.]{1,100}'";
 // Note: substitute s with the month names january, february, etc. Define an enum or something for that.
 // e.g. -p "may 2015"
@@ -53,7 +53,7 @@ int ive_prepare_temp_file(
     {   
         if (i > 0)
             l_current_year++;
-        sprintf(l_cmd, f_cmd_income_vs_expenses_yearly, a_input_file, l_current_year);
+        l_cmd = ive_get_full_cmd(a_plot_timeframe_t, a_input_file, l_current_year); 
         l_fp = popen(l_cmd, "r");
         if (l_fp == NULL)
         {
@@ -99,4 +99,28 @@ int ive_prepare_temp_file(
         fprintf(a_output_file, "%d %.2lf %.2lf %.2lf\n", l_current_year, l_d1, l_d2, l_d3); 
     }
     return 0;
+}
+
+char *ive_get_full_cmd(
+    enum a_enum_plot_timeframe_t,
+    char *a_input_file,
+    int a_current_year
+)
+{
+    char *l_result;
+    switch(a_enum_plot_timeframe_t)
+    {
+        case daily:
+            //sprintf(l_result, f_cmd_daiy, a_input_file, l_current_year);
+            break;
+        case weekly:
+            //sprintf(l_result, f_cmd_weekly, a_input_file, l_current_year);
+            break;
+        case monthly:
+            sprintf(l_result, f_cmd_monthly, a_input_file, l_current_year);
+            break;
+        else:
+            sprintf(l_result, f_cmd_yearly, a_input_file, l_current_year);
+    }
+    return l_result;
 }
