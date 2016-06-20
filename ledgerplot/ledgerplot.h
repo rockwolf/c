7ed92863808d179bc5a4dbfa94269d793830d9c6
Andy Nagels <thereisanewway@gmail.com>
2016-06-20 16:42:27 +0200

    Removed, because ledgerplot was moved to its own repo.

--- ledgerplot/ledgerplot.h
+++
@@ -1,9 +0,0 @@
-/* See LICENSE.txt for license and copyright information. */
-#pragma once
-
-#define VERSION "0.1"
-
-enum max_size_t {
-    MS_INPUT_LINE = 1024, /* MAX line length to copy */
-    MS_OUTPUT_ARRAY = 8000 /* MAX command length for the dat info */
-};
